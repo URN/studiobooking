96dc7fe5d30ac61e886d38b56be69614a50f9257
Thomas <thomas@tuomas.co.uk>
2013-07-02 02:30:35 +0100

    ajax form submission, colour events depending on which studio

--- studiobooking.py
+++ studiobooking.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask.ext.wtf import Form
 from flask.ext.sqlalchemy import SQLAlchemy
 from wtforms import TextField, DateField, SelectField, HiddenField, SubmitField
 from datetime import datetime, timedelta
+from json import dumps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+app.config['CALENDAR_COLORS'] = ['#3D862D',  # either
+                                 '#3C76B4',  # studio 1
+                                 '#998333',  # studio 2
+                                 '#733426',  # admin event
+                                 ]
 db = SQLAlchemy(app)
 
 
@@ -27,9 +33,21 @@ class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     contact = db.Column(db.String(120))
-    start = db.Column(db.DateTime)
-    end = db.Column(db.DateTime)
+    start = db.Column(db.Integer)
+    end = db.Column(db.Integer)
     studio = db.Column(db.Integer)
+
+    def as_dict(self):
+        start = datetime.fromtimestamp(self.start).strftime('%Y-%m-%d %H:%M:%S')
+        end = datetime.fromtimestamp(self.end).strftime('%Y-%m-%d %H:%M:%S')
+        start = datetime.fromtimestamp(self.start).isoformat()
+        end = datetime.fromtimestamp(self.end).isoformat()
+        color = app.config['CALENDAR_COLORS'][self.studio]
+        return {'title': self.name,
+                'start': start,
+                'end': end,
+                'allDay': False,
+                'color': color}
 
     def __init__(self, name, contact, start, end, studio=0):
         self.name = name
@@ -68,16 +86,36 @@ def make_booking():
     if request.method == 'POST':
         print form.start.data
         start = form.start.data
-        start = datetime.strptime(form.start.data, '%a, %d %b %Y %H:%M:%S %Z') + timedelta(hours=1)
+        start = datetime.strptime(form.start.data,
+                                  '%a, %d %b %Y %H:%M:%S %Z') + \
+                                  timedelta(hours=1)
         end = datetime.strptime(form.duration.data, '%I:%M%p').time()
         end = datetime.combine(start.date(), end)
-        print start
-        print end
+        print start.strftime('%s')
+        print end.strftime('%s')
         #booking = Booking(form.name.data, form.date.data)
-        booking = Booking(form.name.data, form.contact.data, form.start.data,
-                          form.duration.data, form.studio.data)
-        return 'submitted'
+        booking = Booking(form.name.data, form.contact.data,
+                          start.strftime('%s'),
+                          end.strftime('%s'), form.studio.data)
+        db.session.add(booking)
+        db.session.commit()
+        message = {}
+        message['text'] = 'Submitted!'
+        message['alert'] = 'alert alert-success'
+        return render_template('booking_response.htm', message=message)
     return render_template('booking_form.htm', form=form)
+
+
+@app.route('/events')
+def get_events():
+    start = request.args['start']
+    end = request.args['end']
+    events = Booking.query.filter(Booking.start.between(start, end)).all()
+    data = map(lambda x: x.as_dict(), events)
+    print data
+    print events[1].as_dict()
+    return dumps(data)
+    return jsonify(events[1].as_dict())
 
 
 @app.route('/')
